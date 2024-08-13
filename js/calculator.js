0f06e8caed60d7904415287b1a5dd03bdd46408c
NoRegret <no.regret.ccg@gmail.com>
2024-08-13 09:37:23 +0000

    bux fixes  + linting

--- js/calculator.js
+++ js/calculator.js
@@ -1,23 +1,26 @@
 export default class Calculator {
-    constructor() {
-        this.values = [0, 0]
-        this.activeValue = 0;
+  constructor() {
+    this.values = [0, 0];
+    this.activeValue = 0;
 
-        this.operation = '';
+    this.operation = "";
+  }
+
+  updateValue(newValue) {
+    if (this.values[this.activeValue].toString().length < 15) {
+      this.values[this.activeValue] =
+        this.values[this.activeValue] * 10 + newValue;
     }
+  }
 
-    updateValue(newValue) {
-        this.values[this.activeValue] = this.values[this.activeValue] * 10 + newValue;
-    }
+  setOperation(operation) {
+    this.operation = operation;
+  }
 
-    setOperation(operation) {
-        this.operation = operation;
-    }
+  clearCalculator() {
+    this.values = [0, 0];
+    this.activeValue = 0;
 
-    clearCalculator() {
-        this.values = [0, 0]
-        this.activeValue = 0;
-
-        this.operation = '';
-    }
+    this.operation = "";
+  }
 }
